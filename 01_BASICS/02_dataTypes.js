691a43a2ff599cb97d65cf32f4dc039330c9f617
ASHISH KUMAR <145495068+IamAshishSingh02@users.noreply.github.com>
2025-06-07 19:34:33 +0000

    learned about dataTypes

---
+++ 01_BASICS/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // Treats all JS code as newer version[modular code]
+
+// alert("Hello") // Reason for not running- Using NodeJS, not Browser.
+
+let name = "Ashish"
+let age = 21
+let isLoggedIn = true
+let state
+
+// number -> 2^53
+// bigint -> very large values
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined
+// symbol -> unique
+
+// object[Maharathi]
+
+console.log(typeof "Ashish")
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
