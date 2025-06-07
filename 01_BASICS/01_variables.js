92fbcc8a5a847d56ebe50ff0a4377c4096ec2fef
ASHISH KUMAR <145495068+IamAshishSingh02@users.noreply.github.com>
2025-06-07 19:07:04 +0000

    learned about variables

---
+++ 01_BASICS/01_variables.js
@@ -0,0 +1,18 @@
+/*
+Prefer not ot use var
+because of issue in block scope and functional scope
+*/
+
+const accId = 12434
+let accEmail = "fgegfrf@gmail.com"
+var accPass = "3143245"
+accCity = "weegb" //This one is wrost.
+let accState
+
+//accId = 23
+accEmail = "fvcvdf"
+accPass = "4324"
+accCity = "fe"
+
+//console.log(accId);
+console.table([accId, accEmail, accPass, accCity, accState])
