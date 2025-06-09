492cc8a3b4e997b7f77dda737361a3c504da1a6f
ASHISH KUMAR <145495068+IamAshishSingh02@users.noreply.github.com>
2025-06-09 11:32:43 +0000

    learned about numbers

---
+++ 01_BASICS/07_numsAndMaths.js
@@ -0,0 +1,11 @@
+const score = 400
+const bal = new Number(100)
+
+console.log(score)
+console.log(bal)
+
+console.log(bal.toString().length)
+console.log(bal.toFixed(2))
+
+const num = 23.8966
+console.log(num.toPrecision(3))
