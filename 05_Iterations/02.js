88afc9587bd2e0d39332f35ec6e2e0e0a4e27018
ASHISH KUMAR <145495068+IamAshishSingh02@users.noreply.github.com>
2025-06-19 13:39:42 +0000

    learned about while loop

---
+++ 05_Iterations/02.js
@@ -0,0 +1,20 @@
+let index = 0
+while(index <= 10){
+    // console.log(`Value of index is ${index}`)
+    index += 2
+}
+
+let myArr = ["superman", "batman", "ironman"]
+let idx = 0
+while(idx < myArr.length){
+    // console.log(`Value is ${myArr[idx]}`)
+    idx += 1
+}
+
+// Kam phele hoga condition bad mein check hogi
+let score = 11
+do {
+    // console.log(`Score is ${score}`)
+    score++
+}
+while(score <= 10)
