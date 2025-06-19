3f63f82f2b32700f6ed14499658b2dbff9a1d7ac
ASHISH KUMAR <145495068+IamAshishSingh02@users.noreply.github.com>
2025-06-19 14:00:35 +0000

    learned about forof loop

---
+++ 05_Iterations/03.js
@@ -0,0 +1,30 @@
+// for of loop
+
+const arr = [1, 2, 3, 4, 5]
+for(const num of arr){
+    //console.log(num)
+}
+
+const greetings = "Hello world!"
+for(const greet of greetings){
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('RUS', "Russia")
+map.set('USA', "United States of America")
+console.log(map)
+for(const [key, value] of map){
+   console.log(`${key}:-${value}`)
+}
+
+const myObj = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+// for(const [key, value] of myObj){
+//     console.log(`${key}:-${value}`)
+// }
