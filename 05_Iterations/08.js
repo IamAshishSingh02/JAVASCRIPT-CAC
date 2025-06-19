8f326d71ed3b4b84ea39cfac1515ad26e7cd185a
ASHISH KUMAR <145495068+IamAshishSingh02@users.noreply.github.com>
2025-06-19 17:34:34 +0000

    learned about .reduce

---
+++ 05_Iterations/08.js
@@ -0,0 +1,27 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currVal){
+//     console.log(`acc: ${acc} and currVal: ${currVal}`)
+//     return acc + currVal
+// }, 0)
+// console.log(myTotal)
+
+const myTotal = myNums.reduce((acc, currVal) => acc + currVal, 0)
+console.log(myTotal)
+
+const shoppingCart = [
+    {
+        itemName: 'js course',
+        price: 2999
+    },
+    {
+        itemName: 'py course',
+        price: 999
+    },
+    {
+        itemName: 'app dev course',
+        price: 1999
+    }
+]
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+console.log(priceToPay)
